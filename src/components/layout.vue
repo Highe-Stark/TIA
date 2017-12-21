42ab0ab6384f516949067f53634a7b0ed17b9136
Higher <higherwashington@gmail.com>
2017-12-21 19:31:37 +0800

    Revert layout.vue to workable version

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="layout" class="container">
     <div class="alert alert-primary">Comment</div>
-    <div v-for="cmt in comments" v-bind:key="cmt.cmtWord">
-      <!--<div class="card cmt" style="height: 8rem">
+    <span class="cmt">
+      <p>
+      <Comment></Comment>
+      </p>
+    </span>
+    <a v-for="cmt in comments" v-bind:key="cmt.cmtWord">
+      <div class="card cmt" style="height: 8rem">
         <img src="../assets/logo.png" alt="face" class="card-img-top img-circle cmt" height="30rem" width="30rem">
         <div class="card-body">
           <p class="card-text">{{cmt.cmtWord}}</p>
         </div>
-      </div>-->
-      <comment :comment='cmt'></comment>
-    </div>
+      </div>
+    </a>
     <div class="container">
       <textarea class="form-control" id="user-cmt" placeholder="Give your comment here..."></textarea>
       <button class="btn btn-primary" v-on:click="issue">Submit</button>
@@ -18,25 +22,26 @@
 </template>
 
 <script>
-import comment from './comment'
+import Comment from './comment'
 export default {
   name: 'layout',
   components: {
-    comment
+    Comment
+  },
+  data: {
+    comments:[
+      { img: "@/assets/logo.png", cmtWord: "This class is good" },
+      { img: "@/assets/logo.png", cmtWord: "Fantastic!"}
+    ]
   },
   data() {
-    return {
-      comments:[
-        { fimg: "@/assets/logo.png", fcmt: "This class is good", Sup: 0, Opp: 0 },
-        { fimg: "@/assets/logo.png", fcmt: "Fantastic!", Sup: 0, Opp: 0 }
-      ]
-    }
+    return this.comments;
   },
   methods: {
     issue() {
       let t = document.getElementById("user-cmt");
-      this.comments.push({ fimg: "@/assets/logo.png", fcmt: t.value, Sup: 0, Opp: 0});
-      t.value=""
+      console.log(t.value);
+      this.comments.push({ img: "@/assets/logo.png", cmtWord: t.value})
     }
   }
 }
@@ -46,7 +51,7 @@ export default {
 #layout {
   padding-right: 0.25rem;
   padding-left: 0.25rem;
-  background-color:rgb(175, 247, 247);
+  background-color:aquamarine;
 }
 h1 {
   font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
