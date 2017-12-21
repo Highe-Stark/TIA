280db6e4646262565668862f64b4e95e2bd39267
Higher <higherwashington@gmail.com>
2017-12-21 12:25:04 +0800

    modified

--- src/components/comment.vue
+++ src/components/comment.vue
@@ -2,8 +2,8 @@
   <div app=Comment>
     <!--<h1>Comment Pane</h1>-->
     <!--<p>Welcome to comment pane, make comments about course/speciality here</p>-->
-    <img src="../assets/face.png" alt="face" height="36rem" width="36rem"/>
-    <p>{{comment}}</p>
+    <img src="../assets/face.png" alt="face" height="36rem" width="36rem" class="img-circle">
+    <p class="Card">{{comment}}</p>
     <!--<div id="cmt">Comments</div>-->
     <router-view/>
   </div>
@@ -12,6 +12,9 @@
 <script>
 export default {
   name: 'Comment',
+  props: [
+    'img', 'text'
+  ],
   data() {
     return { comment: "Good" }
   }
--- src/components/layout.vue
+++ src/components/layout.vue
@@ -1,11 +1,23 @@
 <template>
-  <div id="layout">
-    <h1>{{greet}}</h1>
+  <div id="layout" class="container">
+    <div class="alert alert-primary">Comment</div>
     <span class="cmt">
       <p>
       <Comment></Comment>
       </p>
     </span>
+    <a v-for="cmt in comments" v-bind:key="cmt.cmtWord">
+      <div class="card cmt" style="height: 8rem">
+        <img src="../assets/logo.png" alt="face" class="card-img-top img-circle cmt" height="30rem" width="30rem">
+        <div class="card-body">
+          <p class="card-text">{{cmt.cmtWord}}</p>
+        </div>
+      </div>
+    </a>
+    <div class="container">
+      <textarea class="form-control" id="user-cmt" placeholder="Give your comment here..."></textarea>
+      <button class="btn btn-primary" v-on:click="issue">Submit</button>
+    </div>
   </div>
 </template>
 
@@ -16,23 +28,39 @@ export default {
   components: {
     Comment
   },
+  data: {
+    comments:[
+      { img: "@/assets/logo.png", cmtWord: "This class is good" },
+      { img: "@/assets/logo.png", cmtWord: "Fantastic!"}
+    ]
+  },
   data() {
-    return {"greet": "Welcome!"}
+    return this.comments;
+  },
+  methods: {
+    issue() {
+      let t = document.getElementById("user-cmt");
+      console.log(t.value);
+      this.comments.push({ img: "@/assets/logo.png", cmtWord: t.value})
+    }
   }
 }
 </script>
 
 <style>
-#id {
-  margin: 0.3rem;
-  padding: 0.12rem;
+#layout {
+  padding-right: 0.25rem;
+  padding-left: 0.25rem;
   background-color:aquamarine;
 }
 h1 {
   font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  color: red
+  color: ivory;
+  background-color: aqua;
 }
 .cmt {
   text-align: left;
+  padding:0.5rem;
+  font-size: large;
 }
 </style>
