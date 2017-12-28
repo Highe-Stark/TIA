8cd8b088e2e970bf3ae401d3fe1d7afefda300a4
Higher <higherwashington@gmail.com>
2017-12-28 16:53:15 +0800

    Scope CSS style in comment-pane

--- src/components/children/layout.vue
+++ src/components/children/layout.vue
@@ -75,7 +75,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 #container {
   min-height: 100%;
   height: auto;
--- src/components/layout.vue
+++ src/components/layout.vue
@@ -75,7 +75,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 #container1 {
   min-height: 100%;
   height: auto;
